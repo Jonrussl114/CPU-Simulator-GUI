4f7a799bd9446bdccad86e59853e4ca82a6f771e
Jonrussl114 <jonrussl114@gmail.com>
2025-04-28 04:03:21 -0400

    Added code for buttons to the new algorithms

--- CpuSchedulingWinForms/CpuScheduler.cs
+++ CpuSchedulingWinForms/CpuScheduler.cs
@@ -359,5 +359,128 @@ namespace CpuSchedulingWinForms
         {
             this.txtCodeInput.Clear();
         }
+
+        private void btnSRFT_Click(object sender, EventArgs e)
+        {
+            if (txtProcess.Text != "")
+            {
+                var results = Algorithms.srtfAlgorithm(txtProcess.Text);
+                int numberOfProcess = Int16.Parse(txtProcess.Text);
+                if (numberOfProcess <= 10)
+                {
+                    this.progressBar1.Increment(4); //cpu progress bar
+                    this.progressBar1.SetState(1);
+                    this.progressBar2.Increment(13);
+                    this.progressBar2.SetState(1);
+                }
+                else if (numberOfProcess > 10)
+                {
+                    this.progressBar1.Increment(15);
+                    this.progressBar1.SetState(1);
+                    this.progressBar2.Increment(38); //memory progress bar
+                    this.progressBar2.SetState(3);
+                }
+
+                listView1.Clear();
+                listView1.View = View.Details;
+
+                listView1.Columns.Add("Process ID", 150, HorizontalAlignment.Center);
+                listView1.Columns.Add("Arrival Time", 100, HorizontalAlignment.Center);
+                listView1.Columns.Add("Burst Time", 100, HorizontalAlignment.Center);
+                listView1.Columns.Add("Predicted Time", 100, HorizontalAlignment.Center);
+                dgvResults.DataSource = results;
+                dgvResults.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+                dgvResults.ReadOnly = true;
+                dgvResults.AllowUserToAddRows = false;
+                dgvResults.Visible = true;
+                HideTable.Visible = true;
+                dgvResults.Columns["RR"].Visible = false;
+                dgvResults.Columns["PT"].Visible = true;
+
+                for (int i = 0; i < numberOfProcess; i++)
+                {
+                    var item = new ListViewItem();
+                    item.Text = "Process " + (i + 1);
+                    item.SubItems.Add("-");
+                    listView1.Items.Add(item);
+                }
+
+                listView1.Items.Add("\n");
+                listView1.Items.Add("CPU handles: " + numberOfProcess);
+            }
+            else
+            {
+                MessageBox.Show("Enter number of processes", "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtProcess.Focus();
+            }
+        }
+
+        private void btnHRRN_Click(object sender, EventArgs e)
+        {
+            if (txtProcess.Text != "")
+            {
+                var results = Algorithms.hrrnAlgorithm(txtProcess.Text);
+                int numberOfProcess = Int16.Parse(txtProcess.Text);
+                if (numberOfProcess <= 10)
+                {
+                    this.progressBar1.Increment(4); //cpu progress bar
+                    this.progressBar1.SetState(1);  //cpu color progress bar
+                    this.progressBar2.Increment(13);
+                    this.progressBar2.SetState(1);
+                }
+                else if (numberOfProcess > 10)
+                {
+                    this.progressBar1.Increment(15);
+                    this.progressBar1.SetState(1);
+                    this.progressBar2.Increment(38); //memory progress bar
+                    this.progressBar2.SetState(3);   //memory color progress bar
+                }
+                string quantumTime = Helper.QuantumTime;
+                listView1.Clear();
+                listView1.View = View.Details;
+
+                listView1.Columns.Add("Process ID", 150, HorizontalAlignment.Center);
+                listView1.Columns.Add("Burst Time", 100, HorizontalAlignment.Center);
+                dgvResults.DataSource = results;
+                dgvResults.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+                dgvResults.ReadOnly = true;
+                dgvResults.AllowUserToAddRows = false;
+                dgvResults.Visible = true;
+                HideTable.Visible = true;
+                dgvResults.Columns["PT"].Visible = false;
+                dgvResults.Columns["RR"].Visible = true;
+
+                
+
+                for (int i = 0; i < numberOfProcess; i++)
+                {
+                    var item = new ListViewItem();
+                    item.Text = "Process " + (i + 1);
+                    item.SubItems.Add(quantumTime);
+                    listView1.Items.Add(item);
+                }
+
+                listView1.Items.Add("\n");
+                listView1.Items.Add("CPU handles: " + numberOfProcess);
+            }
+            else
+            {
+                MessageBox.Show("Enter number of processes", "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtProcess.Focus();
+            }
+            
+        
+        }
+
+        private void dgvResults_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void HideTable_Click(object sender, EventArgs e)
+        {
+            dgvResults.Visible = false;
+            HideTable.Visible = false;
+        }
     }
 }
